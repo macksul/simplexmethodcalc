f3f51dd2e1c2cf85abe05e150f8abf393efbee47
Ahmad Suleiman <ahmadsuleimanbusiness@gmail.com>
2023-07-15 18:22:04 -0700

    created requirements, post method, and simplex cal

--- static/js/home.js
+++ static/js/home.js
@@ -1,23 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const display = document.querySelector('.display');
-    const buttons = document.querySelectorAll('.calculator button');
-  
-    buttons.forEach(function(button) {
-      button.addEventListener('click', function() {
-        const value = button.textContent;
-  
-        if (value === '=') {
-          try {
-            display.value = eval(display.value);
-          } catch (error) {
-            display.value = 'Error';
-          }
-        } else if (value === 'C') {
-          display.value = '';
-        } else {
-          display.value += value;
+document.addEventListener('DOMContentLoaded', function () {
+  const display = document.querySelector('.display');
+  const buttons = document.querySelectorAll('.calculator button');
+
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      const value = button.textContent;
+
+      if (value === '=') {
+        try {
+          display.value = eval(display.value);
+        } catch (error) {
+          display.value = 'Error';
         }
-      });
+      } else if (value === 'C') {
+        display.value = '';
+      } else {
+        display.value += value;
+      }
     });
   });
-  
+});
+
+// post data to flask server
+function postData(url = '', data = {}) {
+  return fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+}
