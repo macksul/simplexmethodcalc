e1937996e35d875c5a55001d7262cbcb02c0fe1f
Ahmad Suleiman <ahmadsuleimanbusiness@gmail.com>
2023-07-15 17:14:53 -0700

    Initial creation of Flask directories

---
+++ app.py
@@ -0,0 +1,17 @@
+# Flask app
+
+# Importing simple flask modules
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
+# creating flask app
+app = Flask(__name__)
+
+# Create route for home page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Run app
+if __name__ == '__main__':
+    app.run(debug=True)
+
