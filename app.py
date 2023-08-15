70f94bdd373c6884c55ef0da32088144fc2f44bc
macksul <msulieman@csustan.edu>
2023-08-15 13:24:01 -0700

    updating old python/html code

--- app.py
+++ app.py
@@ -9,9 +9,20 @@ app = Flask(__name__)
 # Create route for home page
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('newhome.html')
 
+@app.route('/', methods=['POST'])
+def get_tableau_from_user():
 
+    dec_vars = request.form.get("dec_vars", type=int, default=0)
+    slack_vars = request.form.get("slack_vars", type=int, default=0)
+    excess_vars = request.form.get("excess_vars", type=int, default=0)
+
+    # 1 = Z column and 2 = RHS and Row Ops
+    n_rows = 1 + slack_vars + excess_vars
+    n_cols =  1 + dec_vars + slack_vars + excess_vars + 2
+    return render_template('home.html', cols = n_cols, rows = n_rows)
+    
 
 # Run app
 if __name__ == '__main__':
