45101c87c0b2112b6f93eb1bcd6946f4c7582bea
macksul <msulieman@csustan.edu>
2023-10-30 11:32:27 -0700

    Cleaned up files

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 # Create route for home page
 @app.route('/')
 def home():
-    return render_template('home2.html')
+    return render_template('home.html')
 
 @app.route('/', methods=['POST'])
 def get_tableau_from_user():
-    return render_template('home2.html')
+    return render_template('home.html')
     
 # Run app
 if __name__ == '__main__':
