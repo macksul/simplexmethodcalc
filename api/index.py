99bbfc21684605a817356b3bba87dfb82f4f36be
MadMTG <60371882+MadSuleiman@users.noreply.github.com>
2024-02-13 22:30:48 -0800

    removed testing return

--- api/index.py
+++ api/index.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 # Create route for home page
 @app.route('/')
 def home():
-    # return render_template('home.html')
-    return "testing"
+    return render_template('home.html')
 
 @app.route('/', methods=['POST'])
 def get_tableau_from_user():
